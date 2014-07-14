17876a07dba7410f3b4f883db184bd98263c6733
mobileappscloud <doc92606@gmail.com>
2014-07-13 23:23:33 -0700

    Initial Commit

---
+++ FindItClose/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FindItClose
+//
+//  Created by iMac on 7/13/14.
+//  Copyright (c) 2014 CongenialApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FindItClose/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FindItClose
+//
+//  Created by iMac on 7/13/14.
+//  Copyright (c) 2014 CongenialApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
